6532863326b85ad48122518811fdc95a1ccba065
MAXIMILIANO GARBARINO <47433817@ort>
2024-08-02 12:07:07 -0300

    finalizado

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import Todo from './Todo';
 
 const Todos = ({ todos, setTodos }) => {
   return (
     <View style={styles.todosContainer}>
-      {todos.map((todo, index) => (
-        <Todo
-          key={index}
-          setTodos={setTodos}
-          todos={todos}
-          todo={todo}
-        />
-      ))}
+      <FlatList
+        data={todos}
+        renderItem={({ item }) => <Todo setTodos={setTodos} todos={todos} todo={item} />}
+        keyExtractor={(item) => item.created.toString()}
+      />
     </View>
   );
 };
@@ -20,7 +17,9 @@ const Todos = ({ todos, setTodos }) => {
 const styles = StyleSheet.create({
   todosContainer: {
     marginTop: 16,
+    flex: 1, // Asegura que el contenedor ocupa todo el espacio disponible
   },
 });
 
 export default Todos;
+
