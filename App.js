a4c6e62c3bb3b053384c03002f665ea6e83429b3
MAXIMILIANO GARBARINO <47433817@ort>
2024-08-02 08:50:24 -0300

    hola

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Tareas from './components/Tareas';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header></Header>
+      <Tareas></Tareas>
     </View>
   );
 }
