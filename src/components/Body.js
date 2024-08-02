e33c7d69e4b04a2e0e4b9c48c687f1b975d7a347
TOMÁS LEONEL DEGESE <47349708@ort>
2024-08-02 10:06:27 -0300

    fin consigna

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import Title from './Title';
 import Form from './Form';
 import Todos from './Todos';
 
-const Body = () => {
-  const [todos, setTodos] = useState([
-  ]);
-
+const Body = ({ todos, setTodos }) => {
   return (
     <View style={styles.container}>
       <Title />
